ee651d76a6ca496bdd5156420f22a9bbfbb8eaa6
Derpinou <lightcompte@gmail.com>
2021-08-30 19:18:00 +0200

    Typescript rewrite

---
+++ index.ts
@@ -0,0 +1,3 @@
+export {Manager} from "./src/Manager"
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const version: string = require(`${__dirname}/../package.json`).version;
---
+++ src/Manager.ts
@@ -0,0 +1,31 @@
+import {EventEmitter} from "events";
+import {Client, GuildMember} from "discord.js";
+import {ManagerOptions} from "./interfaces";
+
+class Manager extends EventEmitter {
+    readonly client: Client
+    readonly config: ManagerOptions
+    constructor(client: Client, options: ManagerOptions) {
+        super();
+        if (!client) throw new Error("Please provide Client")
+        this.client = client
+        if (!options ||  typeof options !== "object") throw new Error("Please provide valid options object")
+        this.config = options
+    }
+
+    async setRole(member: GuildMember) {
+        if (member.user.flags) {
+            let flags = member.user.flags.serialize();
+            console.log(flags)
+            for (const [key, value] of Object.entries(flags)) {
+                // @ts-ignore
+                if (this.config[key] && value) {
+                    // @ts-ignore
+                    await member.roles.add(this.config[key]);
+                }
+            }
+        }
+    }
+}
+
+export {Manager}

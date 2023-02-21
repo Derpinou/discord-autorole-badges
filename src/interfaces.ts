99ec82956eb9e2dfa6077fadf21b8f2442d76ead
Yeikzy <yeikzypro@gmail.com>
2023-02-21 20:58:50 +0100

    Update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,15 +1,16 @@
 export interface ManagerOptions {
-    DISCORD_EMPLOYEE: string
-    PARTNERED_SERVER_OWNER: string
-    HYPESQUAD_EVENTS?: string
-    BUGHUNTER_LEVEL_1?: string
-    HOUSE_BRAVERY?: string
-    HOUSE_BRILLIANCE?: string
-    HOUSE_BALANCE?: string
-    EARLY_SUPPORTER?: string
+    STAFF: string
+    PARTNER: string
+    HYPESQUAD?: string
+    BUG_HUNTER_LEVEL_1?: string
+    HYPESQUAD_ONLINE_HOUSE_1?: string
+    HYPESQUAD_ONLINE_HOUSE_2?: string
+    HYPESQUAD_ONLINE_HOUSE_3?: string
+    PREMIUM_EARLY_SUPPORTER?: string
     TEAM_USER?: string
-    BUGHUNTER_LEVEL_2?: string
+    BUG_HUNTER_LEVEL_2?: string
     VERIFIED_BOT?: string
-    EARLY_VERIFIED_BOT_DEVELOPER?: string
-    DISCORD_CERTIFIED_MODERATOR?: string
+    ACTIVE_DEVELOPER?: string
+    VERIFIED_DEVELOPER?: string
+    CERTIFIED_MODERATOR?: string
 }
